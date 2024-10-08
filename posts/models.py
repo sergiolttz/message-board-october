da91218d53b5859b06f31906fbebf3d7bc2e7f37
sergiolttz <sergiolattanzio05@gmail.com>
2024-10-08 10:33:25 -0400

    model added

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Publication(models.Model):
-    text = models.TextField()
+    text = models.TextField(default="")
 
     def __str__(self) -> str:
         return self.text[:50]
