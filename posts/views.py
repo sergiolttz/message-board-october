44db6e4c574e64ab1a9fedec55a257593dcb75cc
sergiolttz <sergiolattanzio05@gmail.com>
2024-10-10 17:40:51 -0400

    post new added

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Publication
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 class PostListView(ListView):
     model = Publication
     template_name = "post_list.html"
     
+class PostCreateView(CreateView):
+    model = Publication
+    template_name = "post_new.html"
+    fields=["text",]
